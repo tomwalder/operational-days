c002f3ed35126c9a58b4578b2f1437d1a2c66e7d
kchurchill <karl.churchill@gear4music.com>
2018-08-13 14:04:00 +0100

    Reducing interface to minimum immutable version and expanding basic concept.

---
+++ HasOperationalDaysInterface.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * HasOperationalDaysInterface
+ */
+interface HasOperationalDaysInterface {
+
+    /**
+     * Returns an OperationalDaysInterface that describe the OperationalDays for the implementing entity
+     * between the dates provided.
+     *
+     * @param DateTimeInterface $obj_until
+     * @param DateTimeInterface $obj_since - If null, today is assumed
+     * @return OperationalDaysInterface 
+     *
+     * @throws InvalidArgumentException - Raised if the date range is inverted.
+     */
+    public function getOperationalDays(DateTimeInterface $obj_until, DateTimeInterface $obj_since = null);
+}
+
---
+++ OperationalDaysEnum.php
@@ -0,0 +1,34 @@
+<?php
+
+/**
+ * OperationalDaysInterface
+ */
+interface OperationalDaysEnum {
+    const
+        /**
+         * Enumerated days of week. Follows the date('w') convention that Sunday is zero.
+         */
+        DOW_SUN = 0,
+        DOW_MON = 1,
+        DOW_TUE = 2,
+        DOW_WED = 3,
+        DOW_THU = 4,
+        DOW_FRI = 5,
+        DOW_SAT = 6,
+
+        /**
+         * Bitfield values for day of week. These are defined as 1 << DOW_XXX
+         */
+        BF_SUN     =   1,
+        BF_MON     =   2,
+        BF_TUE     =   4,
+        BF_WED     =   8,
+        BF_THU     =  16,
+        BF_FRI     =  32,
+        BF_SAT     =  64,
+        BF_WEEKDAY =  62,
+        BF_WEEKEND =  65,
+        BF_ALL     = 127
+    ;
+}
+
